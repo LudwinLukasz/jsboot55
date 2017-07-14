08e8b4284cae091f8de52d818cf084e7ee954408
Lukasz Ludwin <gorli@interia.pl>
2017-07-14 14:10:36 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '311331261724-a3dq55t70scqi2n7eentr0pr3u8lkm88.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'xbxruKiK2vJOFzWRLLWbF-lz',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
